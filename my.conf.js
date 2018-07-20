e372d1fa00fe9bb6e5255b6cf72540f20ac4b3f8
mrdang <khacquyet.dang.pro@gmail.com>
2018-07-20 08:10:03 +0200

    in progress, add support for mocha

--- my.conf.js
+++ my.conf.js
@@ -1,18 +1,28 @@
+var sourcePreprocessors = 'coverage';
+
+function isDebug(argument) {
+  return argument === '--debug';
+}
+if (process.argv.some(isDebug)) {
+  sourcePreprocessors = [];
+}
+
 module.exports = function (config) {
   config.set({
     basePath: '',
     autoWatch: true,
-    frameworks: ['jasmine'],
-    plugins: ['karma-jasmine', 'karma-coverage', 'karma-phantomjs-launcher'],
+    frameworks: ['mocha', 'sinon-chai'],
+    plugins: ['karma-mocha', 'karma-sinon-chai', 'karma-coverage', 'karma-chrome-launcher'],
     files: [
+      'node_modules/babel-polyfill/dist/polyfill.js',
       'src/*.js',
       'test/spec/*.js'
     ],
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'], // You may use 'ChromeCanary', 'Chromium' or any other supported browser
 
     reporters: ['progress', 'coverage'],
     preprocessors: {
-      'src/*.js': ['coverage']
+      'src/*.js': sourcePreprocessors
     },
 
     singleRun: true
